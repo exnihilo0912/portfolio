f55a190ad4af6c10cc23bd7e9526102fbae058d8
Adam EMMANUEL <adam.emmanuel.pro@gmail.com>
2024-09-18 08:26:42 +0900

    [feat] Add error styling + refactor js error handling

--- packages/2_Junior/showcase_4/src/scripts/index.js
+++ packages/2_Junior/showcase_4/src/scripts/index.js
@@ -1,76 +1,122 @@
 const form = document.querySelector('#contact-form');
+  function isInputValid(inputElement) {
+    return inputElement.validity.valid;
+  }
+  function showInputError(inputSelector, inputErrorSelector) {
+    const classes = document.querySelector(inputSelector).closest(inputErrorSelector)?.className;
+    if (!classes) {
+      console.log(inputErrorSelector);
+      return;
+    }
+    document.querySelector(inputSelector).closest(inputErrorSelector).className = classes + ' ' + 'error';
+  }
+  function hideInputError(inputSelector, inputErrorSelector) {
+    const classes = document.querySelector(inputSelector).closest(inputErrorSelector).className;
+    document.querySelector(inputSelector).querySelector(inputErrorSelector).className = classes.replace('error', '');
+  }
+
+  const formInputs = [
+    {
+      selector: '#firstname',
+      errorSelector: '.input-group',
+      validator: isInputValid,
+      showError: showInputError,
+      hideError: hideInputError,
+    },
+    {
+      selector: '#lastname',
+      errorSelector: '.input-group',
+      validator: isInputValid,
+      showError: showInputError,
+      hideError: hideInputError,
+    },
+    {
+      selector: '#email',
+      errorSelector: '.input-group',
+      validator: isInputValid,
+      showError: showInputError,
+      hideError: hideInputError,
+    },
+    {
+      selector: '#message',
+      errorSelector: '.input-group',
+      validator: isInputValid,
+      showError: showInputError,
+      hideError: hideInputError,
+    },
+    {
+      selector: '#general-enquiry',
+      errorSelector: '.radio-input-group',
+      validator: isInputValid,
+      showError: showInputError,
+      hideError: hideInputError,
+    },
+    {
+      selector: '#consent-checkbox',
+      errorSelector: '.checkbox-input-group',
+      validator: isInputValid,
+      showError: showInputError,
+      hideError: hideInputError,
+    }
+  ];
+
 form.addEventListener('submit', function (event) {
   event.preventDefault();
   const fieldValidities = [];
 
-  const firstnameInput = document.querySelector('#firstname');
-  const firstname = firstnameInput.value;
-  const firstnameClasses = document.querySelector('#firstname + p').className;
-  fieldValidities.push(firstnameInput.validity.valid);
-  if (!firstnameInput.validity.valid) {
-    document.querySelector('#firstname + p').className = firstnameClasses + ' ' + 'active';
-  } else  {
-    document.querySelector('#firstname + p').className = firstnameClasses.replace('active', '');
+
+  for (formInput of formInputs) {
+    const {
+      selector,
+      errorSelector,
+      validator,
+      showError,
+      hideError,
+    } = formInput;
+    const inputElement = document.querySelector(selector);
+    const isValidInput = validator(inputElement);
+
+    fieldValidities.push(isValidInput);
+    if (!isValidInput) {
+      showError(selector, errorSelector);
+    } else  {
+      hideError(selector, errorSelector);
+    }
   }
 
-  const lastnameInput = document.querySelector('#lastname');
-  const lastname = lastnameInput.value;
-  const lastnameClasses = document.querySelector('#lastname + p').className;
-  fieldValidities.push(lastnameInput.validity.valid);
-  if (!lastnameInput.validity.valid) {
-    document.querySelector('#lastname + p').className = lastnameClasses + ' ' + 'active';
-  } else  {
-    document.querySelector('#lastname + p').className = lastnameClasses.replace('active', '');
-  }
-
-  const emailInput = document.querySelector('#email');
-  const email = emailInput.value;
-  const emailClasses = document.querySelector('#email + p').className;
-  fieldValidities.push(emailInput.validity.valid);
-  if (!emailInput.validity.valid) {
-    document.querySelector('#email + p').className = emailClasses + ' ' + 'active';
-  } else  {
-    document.querySelector('#email + p').className = emailClasses.replace('active', '');
-  }
-
-  const generalEnquiryInput = document.querySelector('#general-enquiry');
-  const isGeneralInquiry =  generalEnquiryInput.checked;
-  const supportRequestInput = document.querySelector('#support-request');
-  const isSupportRequest =  supportRequestInput.checked;
-  // const firstnameClasses = document.querySelector('#firstname + p').className;
-  fieldValidities.push(generalEnquiryInput.validity.valid);
-
-  const messageInput = document.querySelector('#message');
-  const message = messageInput.value;
-  const messageClasses = document.querySelector('#message + p').className;
-  fieldValidities.push(messageInput.validity.valid);
-  if (!messageInput.validity.valid) {
-    document.querySelector('#message + p').className = messageClasses + ' ' + 'active';
-  } else  {
-    document.querySelector('#message + p').className = messageClasses.replace('active', '');
-  }
-
-  const hasConsentInput = document.querySelector('#consent-checkbox');
-  const hasConsent = hasConsentInput.checked;
-  // const haConsentClasses = document.querySelector('#consent-checkbox + p').className;
-  fieldValidities.push(hasConsentInput.validity.valid);
-  //   if (!hasConsentInput.validity.valid) {
-  //   document.querySelector('#email + p').className = emailClasses + ' ' + 'active';
+  // const generalEnquiryInput = document.querySelector('#general-enquiry');
+  // const isGeneralInquiry =  generalEnquiryInput.checked;
+  // const supportRequestInput = document.querySelector('#support-request');
+  // const isSupportRequest =  supportRequestInput.checked;
+  // const requestTypeClasses = document.querySelector('.radio-input-list + p').className;
+  // fieldValidities.push(generalEnquiryInput.validity.valid);
+  // if (!generalEnquiryInput.validity.valid) {
+  //   document.querySelector('.radio-input-list + p').className = requestTypeClasses + ' ' + 'active';
   // } else  {
-  //   document.querySelector('#email + p').className = emailClasses.replace('active', '');
+  //   document.querySelector('.radio-input-list + p').className = requestTypeClasses.replace('active', '');
+  // }
+
+  // const hasConsentInput = document.querySelector('#consent-checkbox');
+  // const hasConsent = hasConsentInput.checked;
+  // const haConsentClasses = document.querySelector('#consent-checkbox ~ p').className;
+  // fieldValidities.push(hasConsentInput.validity.valid);
+  //   if (!hasConsentInput.validity.valid) {
+  //   document.querySelector('#consent-checkbox ~ p').className = haConsentClasses + ' ' + 'active';
+  // } else  {
+  //   document.querySelector('#consent-checkbox ~ p').className = haConsentClasses.replace('active', '');
   // }
 
   const areAllFieldsValid = fieldValidities.every((fieldValidity) => fieldValidity);
   console.log({ areAllFieldsValid });
-  console.log({
-    firstname: { value: firstname, isValid: firstnameInput.validity.valid },
-    lastname: { value: lastname, isValid: lastnameInput.validity.valid },
-    email: { value: email, isValid: emailInput.validity.valid },
-    isGeneralInquiry: { value: isGeneralInquiry, isValid: generalEnquiryInput.validity.valid },
-    isSupportRequest: { value: isSupportRequest, isValid: supportRequestInput.validity.valid },
-    message: { value: message, isValid: messageInput.validity.valid },
-    hasConsent: { value: hasConsent, isValid: hasConsentInput.validity.valid },
-  });
+  // console.log({
+  //   firstname: { value: firstname, isValid: firstnameInput.validity.valid },
+  //   lastname: { value: lastname, isValid: lastnameInput.validity.valid },
+  //   email: { value: email, isValid: emailInput.validity.valid },
+  //   isGeneralInquiry: { value: isGeneralInquiry, isValid: generalEnquiryInput.validity.valid },
+  //   isSupportRequest: { value: isSupportRequest, isValid: supportRequestInput.validity.valid },
+  //   message: { value: message, isValid: messageInput.validity.valid },
+  //   hasConsent: { value: hasConsent, isValid: hasConsentInput.validity.valid },
+  // });
 
-  alert('Form sent');
+  // alert('Form sent');
 });
