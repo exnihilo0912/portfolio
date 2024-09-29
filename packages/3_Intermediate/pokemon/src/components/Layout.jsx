dc89bf97034e602a1e2b376b8157eb2529474c3f
adam emmanuel <adam.emmanuel.pro@gmail.com>
2024-09-30 07:45:29 +0900

    [feat] Add go back button to header

--- packages/3_Intermediate/pokemon/src/components/Layout.jsx
+++ packages/3_Intermediate/pokemon/src/components/Layout.jsx
@@ -1,10 +1,13 @@
-import { Link, Outlet } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 function Layout() {
+  const navigate = useNavigate();
+  const isRootPage = window.location.href.replace(/https?:\/\//, '').split('/').length <= 2;
   return (
     <div className='page page--grey'>
       <header className="page__header">
-        <Link to={`pokemons`}>Back</Link>
+        {!isRootPage && (<button onClick={() => navigate(-1)}>⬅</button>)}
         <div>Action</div>
       </header>
       <div className="page__content">
