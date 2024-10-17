a5fe0124309fc2aa2c6fe0acc6f0ba081841b584
adam emmanuel <adam.emmanuel.pro@gmail.com>
2024-10-18 08:43:54 +0900

    [feat] Get machine url

--- packages/3_Intermediate/pokemon/src/components/PokemonMove/index.jsx
+++ packages/3_Intermediate/pokemon/src/components/PokemonMove/index.jsx
@@ -8,13 +8,19 @@ export default function PokemonMove({ move, ...props }) {
   const pokemonMove = usePokemonMove(Number(moveId)) || {};
   const { name: typeName } = pokemonMove?.type || {};
   const learnMethod = version_group_details.at(0).move_learn_method.name;
+  const machine = learnMethod === 'machine'
+    ? pokemonMove?.machines?.find(({ version_group: versionGroup }) => versionGroup === 'red-blue' || versionGroup )
+    : null;
+  const machineIndex = machine?.machine?.url;
   const renderLearnMethod = {
-    'level-up' : `Lv. ${version_group_details.at(0).level_learned_at}`,
-    machine: 'CT ??'
-  }
+    'level-up': `Lv. ${version_group_details.at(0).level_learned_at}`,
+    machine: `TM ${JSON.stringify(machineIndex)}`,
+  };
+
   return (
     <div style={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }} {...props}>
-      <span style={{ textTransform: 'capitalize', fontWeight: '500', flexBasis: '100px' }}>{name}</span>
+      {moveId}
+      <span style={{ textTransform: 'capitalize', fontWeight: '500', flexBasis: '130px' }}>{name}</span>
       <span><PokemonType type={typeName} size='tiny' /></span>
       <small style={{ color: 'hsl(0, 0%, 50%)' }}>{renderLearnMethod[learnMethod]}</small>
     </div>
