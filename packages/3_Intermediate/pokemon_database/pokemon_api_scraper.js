d8177cbf140a444a42dc67c4a6aa184f7835e614
adam emmanuel <adam.emmanuel.pro@gmail.com>
2024-11-18 17:45:59 +0900

    [feat] Get all pokemon data

--- packages/3_Intermediate/pokemon_database/pokemon_api_scraper.js
+++ packages/3_Intermediate/pokemon_database/pokemon_api_scraper.js
@@ -11,7 +11,28 @@ async function delay(seconds) {
   });
 }
 
-async function fetchPokemons(first = 1, last = 251) {
+async function fetchAllPokemons() {
+  let pokemonCount = 0;
+  let nextPage = `${rootUrl}/pokemon/`;
+  let shouldContinue = true;
+  const pokemons = [];
+  while (shouldContinue) {
+    const response = await fetch(nextPage);
+    const result = await response.json();
+    if (!pokemonCount) {
+      pokemonCount = result.count;
+    }
+    pokemons.push(...result.results);
+    nextPage = result.next;
+    if (pokemons.length >= pokemonCount) {
+      shouldContinue = false;
+    }
+    console.log(`Fethcing next page: ${nextPage}`);
+  }
+  fs.writeFileSync(`${__dirname}/data/pokemons.js`, JSON.stringify(pokemons));
+}
+
+async function fetchPokemons(first = 1, last = 1025) {
   const pokemonIndexes = Array(last).fill(0).map((_, index) => index + 1).slice(first - 1);
   const pokemons = [];
   for (const pokemonIndex of pokemonIndexes) {
@@ -19,9 +40,9 @@ async function fetchPokemons(first = 1, last = 251) {
     const result = await fetch(`${rootUrl}/pokemon/${pokemonIndex}`);
     const pokemon = await result.json();
     pokemons.push(pokemon);
-    await delay(3);
+    await delay(1);
   }
   fs.writeFileSync(`${__dirname}/data/pokemons.js`, JSON.stringify(pokemons));
 }
 
-fetchPokemons(1, 1);
+fetchPokemons();
