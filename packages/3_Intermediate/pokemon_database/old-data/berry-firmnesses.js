1e462afb48f00bcf433c5de6a8ace6164a23e68a
adam emmanuel <adam.emmanuel.pro@gmail.com>
2024-11-19 09:10:46 +0900

    [feat] Get all resources urls

---
+++ packages/3_Intermediate/pokemon_database/old-data/berry-firmnesses.js
@@ -0,0 +1,508 @@
+[
+  {
+    "berries": [
+      {
+        "name": "pecha",
+        "url": "https://pokeapi.co/api/v2/berry/3/"
+      },
+      {
+        "name": "pamtre",
+        "url": "https://pokeapi.co/api/v2/berry/32/"
+      },
+      {
+        "name": "belue",
+        "url": "https://pokeapi.co/api/v2/berry/35/"
+      },
+      {
+        "name": "wacan",
+        "url": "https://pokeapi.co/api/v2/berry/38/"
+      },
+      {
+        "name": "tanga",
+        "url": "https://pokeapi.co/api/v2/berry/46/"
+      },
+      {
+        "name": "charti",
+        "url": "https://pokeapi.co/api/v2/berry/47/"
+      },
+      {
+        "name": "chilan",
+        "url": "https://pokeapi.co/api/v2/berry/52/"
+      },
+      {
+        "name": "rowap",
+        "url": "https://pokeapi.co/api/v2/berry/64/"
+      }
+    ],
+    "id": 1,
+    "name": "very-soft",
+    "names": [
+      {
+        "language": {
+          "name": "ja-Hrkt",
+          "url": "https://pokeapi.co/api/v2/language/1/"
+        },
+        "name": "とてもやわらかい"
+      },
+      {
+        "language": {
+          "name": "zh-Hant",
+          "url": "https://pokeapi.co/api/v2/language/4/"
+        },
+        "name": "很柔軟"
+      },
+      {
+        "language": {
+          "name": "fr",
+          "url": "https://pokeapi.co/api/v2/language/5/"
+        },
+        "name": "Très tendre"
+      },
+      {
+        "language": {
+          "name": "es",
+          "url": "https://pokeapi.co/api/v2/language/7/"
+        },
+        "name": "Muy blanda"
+      },
+      {
+        "language": {
+          "name": "en",
+          "url": "https://pokeapi.co/api/v2/language/9/"
+        },
+        "name": "Very Soft"
+      },
+      {
+        "language": {
+          "name": "zh-Hans",
+          "url": "https://pokeapi.co/api/v2/language/12/"
+        },
+        "name": "很柔软"
+      }
+    ]
+  },
+  {
+    "berries": [
+      {
+        "name": "cheri",
+        "url": "https://pokeapi.co/api/v2/berry/1/"
+      },
+      {
+        "name": "figy",
+        "url": "https://pokeapi.co/api/v2/berry/11/"
+      },
+      {
+        "name": "iapapa",
+        "url": "https://pokeapi.co/api/v2/berry/15/"
+      },
+      {
+        "name": "bluk",
+        "url": "https://pokeapi.co/api/v2/berry/17/"
+      },
+      {
+        "name": "grepa",
+        "url": "https://pokeapi.co/api/v2/berry/25/"
+      },
+      {
+        "name": "tamato",
+        "url": "https://pokeapi.co/api/v2/berry/26/"
+      },
+      {
+        "name": "rabuta",
+        "url": "https://pokeapi.co/api/v2/berry/29/"
+      },
+      {
+        "name": "spelon",
+        "url": "https://pokeapi.co/api/v2/berry/31/"
+      },
+      {
+        "name": "watmel",
+        "url": "https://pokeapi.co/api/v2/berry/33/"
+      },
+      {
+        "name": "passho",
+        "url": "https://pokeapi.co/api/v2/berry/37/"
+      },
+      {
+        "name": "rindo",
+        "url": "https://pokeapi.co/api/v2/berry/39/"
+      },
+      {
+        "name": "chople",
+        "url": "https://pokeapi.co/api/v2/berry/41/"
+      },
+      {
+        "name": "shuca",
+        "url": "https://pokeapi.co/api/v2/berry/43/"
+      },
+      {
+        "name": "payapa",
+        "url": "https://pokeapi.co/api/v2/berry/45/"
+      },
+      {
+        "name": "haban",
+        "url": "https://pokeapi.co/api/v2/berry/49/"
+      },
+      {
+        "name": "lansat",
+        "url": "https://pokeapi.co/api/v2/berry/58/"
+      },
+      {
+        "name": "micle",
+        "url": "https://pokeapi.co/api/v2/berry/61/"
+      },
+      {
+        "name": "jaboca",
+        "url": "https://pokeapi.co/api/v2/berry/63/"
+      }
+    ],
+    "id": 2,
+    "name": "soft",
+    "names": [
+      {
+        "language": {
+          "name": "ja-Hrkt",
+          "url": "https://pokeapi.co/api/v2/language/1/"
+        },
+        "name": "やわらかい"
+      },
+      {
+        "language": {
+          "name": "zh-Hant",
+          "url": "https://pokeapi.co/api/v2/language/4/"
+        },
+        "name": "柔軟"
+      },
+      {
+        "language": {
+          "name": "fr",
+          "url": "https://pokeapi.co/api/v2/language/5/"
+        },
+        "name": "Tendre"
+      },
+      {
+        "language": {
+          "name": "es",
+          "url": "https://pokeapi.co/api/v2/language/7/"
+        },
+        "name": "Blanda"
+      },
+      {
+        "language": {
+          "name": "en",
+          "url": "https://pokeapi.co/api/v2/language/9/"
+        },
+        "name": "Soft"
+      },
+      {
+        "language": {
+          "name": "zh-Hans",
+          "url": "https://pokeapi.co/api/v2/language/12/"
+        },
+        "name": "柔软"
+      }
+    ]
+  },
+  {
+    "berries": [
+      {
+        "name": "rawst",
+        "url": "https://pokeapi.co/api/v2/berry/4/"
+      },
+      {
+        "name": "persim",
+        "url": "https://pokeapi.co/api/v2/berry/8/"
+      },
+      {
+        "name": "wiki",
+        "url": "https://pokeapi.co/api/v2/berry/12/"
+      },
+      {
+        "name": "mago",
+        "url": "https://pokeapi.co/api/v2/berry/13/"
+      },
+      {
+        "name": "pinap",
+        "url": "https://pokeapi.co/api/v2/berry/20/"
+      },
+      {
+        "name": "kelpsy",
+        "url": "https://pokeapi.co/api/v2/berry/22/"
+      },
+      {
+        "name": "qualot",
+        "url": "https://pokeapi.co/api/v2/berry/23/"
+      },
+      {
+        "name": "hondew",
+        "url": "https://pokeapi.co/api/v2/berry/24/"
+      },
+      {
+        "name": "cornn",
+        "url": "https://pokeapi.co/api/v2/berry/27/"
+      },
+      {
+        "name": "magost",
+        "url": "https://pokeapi.co/api/v2/berry/28/"
+      },
+      {
+        "name": "durin",
+        "url": "https://pokeapi.co/api/v2/berry/34/"
+      },
+      {
+        "name": "kebia",
+        "url": "https://pokeapi.co/api/v2/berry/42/"
+      },
+      {
+        "name": "kasib",
+        "url": "https://pokeapi.co/api/v2/berry/48/"
+      },
+      {
+        "name": "apicot",
+        "url": "https://pokeapi.co/api/v2/berry/57/"
+      },
+      {
+        "name": "enigma",
+        "url": "https://pokeapi.co/api/v2/berry/60/"
+      }
+    ],
+    "id": 3,
+    "name": "hard",
+    "names": [
+      {
+        "language": {
+          "name": "ja-Hrkt",
+          "url": "https://pokeapi.co/api/v2/language/1/"
+        },
+        "name": "かたい"
+      },
+      {
+        "language": {
+          "name": "zh-Hant",
+          "url": "https://pokeapi.co/api/v2/language/4/"
+        },
+        "name": "堅硬"
+      },
+      {
+        "language": {
+          "name": "fr",
+          "url": "https://pokeapi.co/api/v2/language/5/"
+        },
+        "name": "Ferme"
+      },
+      {
+        "language": {
+          "name": "es",
+          "url": "https://pokeapi.co/api/v2/language/7/"
+        },
+        "name": "Dura"
+      },
+      {
+        "language": {
+          "name": "en",
+          "url": "https://pokeapi.co/api/v2/language/9/"
+        },
+        "name": "Hard"
+      },
+      {
+        "language": {
+          "name": "zh-Hans",
+          "url": "https://pokeapi.co/api/v2/language/12/"
+        },
+        "name": "坚硬"
+      }
+    ]
+  },
+  {
+    "berries": [
+      {
+        "name": "leppa",
+        "url": "https://pokeapi.co/api/v2/berry/6/"
+      },
+      {
+        "name": "sitrus",
+        "url": "https://pokeapi.co/api/v2/berry/10/"
+      },
+      {
+        "name": "razz",
+        "url": "https://pokeapi.co/api/v2/berry/16/"
+      },
+      {
+        "name": "nanab",
+        "url": "https://pokeapi.co/api/v2/berry/18/"
+      },
+      {
+        "name": "pomeg",
+        "url": "https://pokeapi.co/api/v2/berry/21/"
+      },
+      {
+        "name": "yache",
+        "url": "https://pokeapi.co/api/v2/berry/40/"
+      },
+      {
+        "name": "coba",
+        "url": "https://pokeapi.co/api/v2/berry/44/"
+      },
+      {
+        "name": "liechi",
+        "url": "https://pokeapi.co/api/v2/berry/53/"
+      },
+      {
+        "name": "ganlon",
+        "url": "https://pokeapi.co/api/v2/berry/54/"
+      },
+      {
+        "name": "salac",
+        "url": "https://pokeapi.co/api/v2/berry/55/"
+      },
+      {
+        "name": "petaya",
+        "url": "https://pokeapi.co/api/v2/berry/56/"
+      }
+    ],
+    "id": 4,
+    "name": "very-hard",
+    "names": [
+      {
+        "language": {
+          "name": "ja-Hrkt",
+          "url": "https://pokeapi.co/api/v2/language/1/"
+        },
+        "name": "とてもかたい"
+      },
+      {
+        "language": {
+          "name": "zh-Hant",
+          "url": "https://pokeapi.co/api/v2/language/4/"
+        },
+        "name": "很堅硬"
+      },
+      {
+        "language": {
+          "name": "fr",
+          "url": "https://pokeapi.co/api/v2/language/5/"
+        },
+        "name": "Très ferme"
+      },
+      {
+        "language": {
+          "name": "es",
+          "url": "https://pokeapi.co/api/v2/language/7/"
+        },
+        "name": "Muy dura"
+      },
+      {
+        "language": {
+          "name": "en",
+          "url": "https://pokeapi.co/api/v2/language/9/"
+        },
+        "name": "Very Hard"
+      },
+      {
+        "language": {
+          "name": "zh-Hans",
+          "url": "https://pokeapi.co/api/v2/language/12/"
+        },
+        "name": "很坚硬"
+      }
+    ]
+  },
+  {
+    "berries": [
+      {
+        "name": "chesto",
+        "url": "https://pokeapi.co/api/v2/berry/2/"
+      },
+      {
+        "name": "aspear",
+        "url": "https://pokeapi.co/api/v2/berry/5/"
+      },
+      {
+        "name": "oran",
+        "url": "https://pokeapi.co/api/v2/berry/7/"
+      },
+      {
+        "name": "lum",
+        "url": "https://pokeapi.co/api/v2/berry/9/"
+      },
+      {
+        "name": "aguav",
+        "url": "https://pokeapi.co/api/v2/berry/14/"
+      },
+      {
+        "name": "wepear",
+        "url": "https://pokeapi.co/api/v2/berry/19/"
+      },
+      {
+        "name": "nomel",
+        "url": "https://pokeapi.co/api/v2/berry/30/"
+      },
+      {
+        "name": "occa",
+        "url": "https://pokeapi.co/api/v2/berry/36/"
+      },
+      {
+        "name": "colbur",
+        "url": "https://pokeapi.co/api/v2/berry/50/"
+      },
+      {
+        "name": "babiri",
+        "url": "https://pokeapi.co/api/v2/berry/51/"
+      },
+      {
+        "name": "starf",
+        "url": "https://pokeapi.co/api/v2/berry/59/"
+      },
+      {
+        "name": "custap",
+        "url": "https://pokeapi.co/api/v2/berry/62/"
+      }
+    ],
+    "id": 5,
+    "name": "super-hard",
+    "names": [
+      {
+        "language": {
+          "name": "ja-Hrkt",
+          "url": "https://pokeapi.co/api/v2/language/1/"
+        },
+        "name": "とてつもなくかたい"
+      },
+      {
+        "language": {
+          "name": "zh-Hant",
+          "url": "https://pokeapi.co/api/v2/language/4/"
+        },
+        "name": "非常堅硬"
+      },
+      {
+        "language": {
+          "name": "fr",
+          "url": "https://pokeapi.co/api/v2/language/5/"
+        },
+        "name": "Super ferme"
+      },
+      {
+        "language": {
+          "name": "es",
+          "url": "https://pokeapi.co/api/v2/language/7/"
+        },
+        "name": "Superdura"
+      },
+      {
+        "language": {
+          "name": "en",
+          "url": "https://pokeapi.co/api/v2/language/9/"
+        },
+        "name": "Super Hard"
+      },
+      {
+        "language": {
+          "name": "zh-Hans",
+          "url": "https://pokeapi.co/api/v2/language/12/"
+        },
+        "name": "非常坚硬"
+      }
+    ]
+  }
+]
--- packages/3_Intermediate/pokemon_database/pokemon_api_scraper.js
+++ packages/3_Intermediate/pokemon_database/pokemon_api_scraper.js
@@ -45,4 +45,177 @@ async function fetchPokemons(first = 1, last = 1025) {
   fs.writeFileSync(`${__dirname}/data/pokemons.js`, JSON.stringify(pokemons));
 }
 
-fetchPokemons();
+async function fetchAbilites(first = 1, last = 307) {
+  const abilityIndexes = Array(last).fill(0).map((_, index) => index + 1).slice(first - 1);
+  const abilities = [];
+  for (const abilityIndex of abilityIndexes) {
+    console.log(`Ability #${abilityIndex}`);
+    const result = await fetch(`${rootUrl}/ability/${abilityIndex}`);
+    const ability = await result.json();
+    abilities.push(ability);
+    await delay(1);
+  }
+  fs.writeFileSync(`${__dirname}/data/abilities.js`, JSON.stringify(abilities));
+}
+
+async function fetchBerries(first = 1, last = 64) {
+  const berryIndexes = Array(last).fill(0).map((_, index) => index + 1).slice(first - 1);
+  const berries = [];
+  for (const berryIndex of berryIndexes) {
+    console.log(`Berry #${berryIndex}`);
+    const result = await fetch(`${rootUrl}/berry/${berryIndex}`);
+    const berry = await result.json();
+    berries.push(berry);
+    await delay(1);
+  }
+  fs.writeFileSync(`${__dirname}/data/berries.js`, JSON.stringify(berries));
+}
+
+async function fetchBerryFirmness(first = 1, last = 5) {
+  const berryFirmnessIndexes = Array(last).fill(0).map((_, index) => index + 1).slice(first - 1);
+  const berryFirmnesses = [];
+  for (const berryFirmnessIndex of berryFirmnessIndexes) {
+    console.log(`Berry Firmness #${berryFirmnessIndex}`);
+    const result = await fetch(`${rootUrl}/berry-firmness/${berryFirmnessIndex}`);
+    const berryFirmness = await result.json();
+    berryFirmnesses.push(berryFirmness);
+    await delay(1);
+  }
+  fs.writeFileSync(`${__dirname}/data/berry-firmnesses.js`, JSON.stringify(berryFirmnesses));
+}
+
+async function fetchBerryFlavor(first = 1, last = 5) {
+  const berryFlavorIndexes = Array(last).fill(0).map((_, index) => index + 1).slice(first - 1);
+  const berryFlavors = [];
+  for (const berryFlavorIndex of berryFlavorIndexes) {
+    console.log(`Berry Flavor #${berryFlavorIndex}`);
+    const result = await fetch(`${rootUrl}/berry-firmness/${berryFlavorIndex}`);
+    const berryFlavor = await result.json();
+    berryFlavors.push(berryFlavor);
+    await delay(1);
+  }
+  fs.writeFileSync(`${__dirname}/data/berry-flavor.js`, JSON.stringify(berryFlavors));
+}
+
+async function fetchCharacteristics(first = 1, last = 30) {
+  const characteristicIndexes = Array(last).fill(0).map((_, index) => index + 1).slice(first - 1);
+  const characteristics = [];
+  for (const characteristicIndex of characteristicIndexes) {
+    console.log(`Characteristic #${characteristicIndex}`);
+    const result = await fetch(`${rootUrl}/characteristic/${characteristicIndex}`);
+    const characteristic = await result.json();
+    characteristics.push(characteristic);
+    await delay(1);
+  }
+  fs.writeFileSync(`${__dirname}/data/characteristic.js`, JSON.stringify(characteristics));
+}
+
+async function fetchEntities(first, last, entityName, entityUrlFragment) {
+  const entityIndexes = Array(last).fill(0).map((_, index) => index + 1).slice(first - 1);
+  const entities = [];
+  for (const entityIndex of entityIndexes) {
+    console.log(`${entityName} #${entityIndex}`);
+    const result = await fetch(`${rootUrl}/${entityUrlFragment}/${entityIndex}`);
+    const entity = await result.json();
+    entities.push(entity);
+    await delay(1);
+  }
+  fs.writeFileSync(`${__dirname}/data/${entityUrlFragment}.js`, JSON.stringify(entities));
+}
+
+// fetchPokemons();
+// fetchAbilites();
+// fetchBerries();
+// fetchBerryFlavor();
+// fetchCharacteristics();
+
+// for (const entity of entities) {
+//   fetchEntities(1, entity.last, entity.entityName, entity.entityUrlFragment);
+// }
+
+const entityUrlByName = {
+  "ability": "https://pokeapi.co/api/v2/ability/",
+  "berry": "https://pokeapi.co/api/v2/berry/",
+  "berry-firmness": "https://pokeapi.co/api/v2/berry-firmness/",
+  "berry-flavor": "https://pokeapi.co/api/v2/berry-flavor/",
+  "characteristic": "https://pokeapi.co/api/v2/characteristic/",
+  "contest-effect": "https://pokeapi.co/api/v2/contest-effect/",
+  "contest-type": "https://pokeapi.co/api/v2/contest-type/",
+  "egg-group": "https://pokeapi.co/api/v2/egg-group/",
+  "encounter-condition": "https://pokeapi.co/api/v2/encounter-condition/",
+  "encounter-condition-value": "https://pokeapi.co/api/v2/encounter-condition-value/",
+  "encounter-method": "https://pokeapi.co/api/v2/encounter-method/",
+  "evolution-chain": "https://pokeapi.co/api/v2/evolution-chain/",
+  "evolution-trigger": "https://pokeapi.co/api/v2/evolution-trigger/",
+  "gender": "https://pokeapi.co/api/v2/gender/",
+  "generation": "https://pokeapi.co/api/v2/generation/",
+  "growth-rate": "https://pokeapi.co/api/v2/growth-rate/",
+  "item": "https://pokeapi.co/api/v2/item/",
+  "item-attribute": "https://pokeapi.co/api/v2/item-attribute/",
+  "item-category": "https://pokeapi.co/api/v2/item-category/",
+  "item-fling-effect": "https://pokeapi.co/api/v2/item-fling-effect/",
+  "item-pocket": "https://pokeapi.co/api/v2/item-pocket/",
+  "language": "https://pokeapi.co/api/v2/language/",
+  "location": "https://pokeapi.co/api/v2/location/",
+  "location-area": "https://pokeapi.co/api/v2/location-area/",
+  "machine": "https://pokeapi.co/api/v2/machine/",
+  "move": "https://pokeapi.co/api/v2/move/",
+  "move-ailment": "https://pokeapi.co/api/v2/move-ailment/",
+  "move-battle-style": "https://pokeapi.co/api/v2/move-battle-style/",
+  "move-category": "https://pokeapi.co/api/v2/move-category/",
+  "move-damage-class": "https://pokeapi.co/api/v2/move-damage-class/",
+  "move-learn-method": "https://pokeapi.co/api/v2/move-learn-method/",
+  "move-target": "https://pokeapi.co/api/v2/move-target/",
+  "nature": "https://pokeapi.co/api/v2/nature/",
+  "pal-park-area": "https://pokeapi.co/api/v2/pal-park-area/",
+  "pokeathlon-stat": "https://pokeapi.co/api/v2/pokeathlon-stat/",
+  "pokedex": "https://pokeapi.co/api/v2/pokedex/",
+  "pokemon": "https://pokeapi.co/api/v2/pokemon/",
+  "pokemon-color": "https://pokeapi.co/api/v2/pokemon-color/",
+  "pokemon-form": "https://pokeapi.co/api/v2/pokemon-form/",
+  "pokemon-habitat": "https://pokeapi.co/api/v2/pokemon-habitat/",
+  "pokemon-shape": "https://pokeapi.co/api/v2/pokemon-shape/",
+  "pokemon-species": "https://pokeapi.co/api/v2/pokemon-species/",
+  "region": "https://pokeapi.co/api/v2/region/",
+  "stat": "https://pokeapi.co/api/v2/stat/",
+  "super-contest-effect": "https://pokeapi.co/api/v2/super-contest-effect/",
+  "type": "https://pokeapi.co/api/v2/type/",
+  "version": "https://pokeapi.co/api/v2/version/",
+  "version-group": "https://pokeapi.co/api/v2/version-group/",
+};
+
+async function fetchAll() {
+  const entityObjs = [];
+  let index = 0;
+
+  for (const entityName in entityUrlByName) {
+    const entityUrl = entityUrlByName[entityName];
+    console.log(entityName);
+    const result = await fetch(entityUrl);
+    const { count, next, results } = await result.json();
+    entityObjs[index] = {
+      name: entityName,
+      entityUrl,
+      count,
+      itemUrls: [...(results.map(({ url }) => url))],
+    };
+
+    let nextPage = next;
+    while (nextPage) {
+      console.log(`${entityName}:\t ${nextPage}`);
+      const result = await fetch(nextPage);
+      const { next, results } = await result.json();
+      nextPage = next;
+      entityObjs[index].itemUrls.push(...(results.map(({ url }) => url)));
+      await delay(0.2);
+    }
+
+    index++;
+    await delay(0.5);
+  }
+
+  console.log(entityObjs);
+  fs.writeFileSync(`${__dirname}/data/allResources.json`, JSON.stringify(entityObjs));
+}
+
+fetchAll();
