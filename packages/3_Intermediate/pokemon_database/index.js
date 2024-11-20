e4e615fda4f2b877fc7f777771faedd6aa0aa8a6
adam emmanuel <adam.emmanuel.pro@gmail.com>
2024-11-20 09:00:59 +0900

    [feat] Add basic resolve entity

--- packages/3_Intermediate/pokemon_database/index.js
+++ packages/3_Intermediate/pokemon_database/index.js
@@ -1,15 +1,30 @@
+const fs = require('fs');
 
 async function resolveUrl(url) {
   const [entity, id] = url.split('/').slice(-3, -1);
   console.log({ entity, id });
 }
 
-const testUrls = [
-  'https://pokeapi.co/api/v2/language/1/',
-  'https://pokeapi.co/api/v2/encounter-method/2/',
-  'https://pokeapi.co/api/v2/version-group/11/',
-]
+async function resolveEntity(name, id, depth, shape) {
+  const entityFilePath = `./data/${name}.json`;
+  const doesEntityExist = fs.existsSync(entityFilePath);
+  if (!doesEntityExist) {
+    throw Error('Entity does not exist.');
+  }
 
-for (const url of testUrls) {
-  resolveUrl(url);
+  const entities = require(entityFilePath);
+  const targetEntity = entities.find(({ id: entityId }) => entityId === id)
+  console.log(targetEntity);
 }
+
+// const testUrls = [
+//   'https://pokeapi.co/api/v2/language/1/',
+//   'https://pokeapi.co/api/v2/encounter-method/2/',
+//   'https://pokeapi.co/api/v2/version-group/11/',
+// ]
+
+// for (const url of testUrls) {
+//   resolveUrl(url);
+// }
+
+resolveEntity('version-group', 11);
